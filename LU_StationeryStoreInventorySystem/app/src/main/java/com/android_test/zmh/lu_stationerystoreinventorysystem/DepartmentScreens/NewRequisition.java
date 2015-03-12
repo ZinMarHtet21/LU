4fd7591954a05dd3d16eca1460aaf4bc2be47ee6
ZinMarHtet21 <zinmarhtet21@gmail.com>
2015-03-12 08:41:55 +0800

    new requisition commit

--- LU_StationeryStoreInventorySystem/app/src/main/java/com/android_test/zmh/lu_stationerystoreinventorysystem/DepartmentScreens/NewRequisition.java
+++ LU_StationeryStoreInventorySystem/app/src/main/java/com/android_test/zmh/lu_stationerystoreinventorysystem/DepartmentScreens/NewRequisition.java
@@ -1,16 +1,21 @@
 package com.android_test.zmh.lu_stationerystoreinventorysystem.DepartmentScreens;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.android_test.zmh.lu_stationerystoreinventorysystem.R;
 import java.util.ArrayList;
@@ -22,17 +27,18 @@ import java.util.Map;
 public class NewRequisition extends ActionBarActivity {
 
     ListView new_req_listView;
-    ArrayAdapter mArrayAdapter;
-    ArrayList mItemList = new ArrayList();
+
     Button sendReqBtn, addBtn;
     EditText itemQty;
     Spinner itemSpinner;
     String itemName;
+    ItemsAdapter itemAdapter;
+
     int itemID;
     int qty;
     HashMap<Integer,String> itemMap = new HashMap<Integer,String>();
-
-    List<Integer> itemIDList = new ArrayList<Integer>();
+    ArrayList<TempItem> items= new ArrayList<TempItem>();
+    ArrayList<TempItem> arrayOfItems = new ArrayList<TempItem>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +46,14 @@ public class NewRequisition extends ActionBarActivity {
         setContentView(R.layout.activity_new_requisition);
         addItemToMap();
 
+        itemAdapter = new ItemsAdapter(this, arrayOfItems);
+
+        new_req_listView = (ListView) findViewById(R.id.item_list_lv);
+        new_req_listView.setAdapter(itemAdapter);
+
+
         itemQty = (EditText)findViewById(R.id.item_qty_et);
         itemSpinner = (Spinner)findViewById(R.id.itemlist_spnr);
-
-        new_req_listView = (ListView)findViewById(R.id.item_list_lv);
-        mArrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,mItemList);
-        new_req_listView.setAdapter(mArrayAdapter);
-
 
         itemSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -54,11 +61,7 @@ public class NewRequisition extends ActionBarActivity {
                 itemName = adapter.getItemAtPosition(position).toString();
 
                 for (Map.Entry<Integer, String> entry : itemMap.entrySet()) {
-                    System.out.println(itemName);
-                    System.out.println(entry.getValue());
-
                     if(entry.getValue().equals(itemName)){
-                        System.out.println("SAME");
                         itemID = entry.getKey();
                     }
                }
@@ -76,20 +79,24 @@ public class NewRequisition extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 qty = Integer.parseInt(itemQty.getText().toString());
+                arrayOfItems.add(new TempItem(itemName,qty));
+                itemAdapter.notifyDataSetChanged();
 
-                AlertDialog alertDialog = new AlertDialog.Builder(
-                        NewRequisition.this).create();
 
-                alertDialog.setTitle("Alert Dialog");
-                alertDialog.setMessage(itemID + "\n" + qty);
-                alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        //Toast.makeText(getApplicationContext(), "You clicked on OK", Toast.LENGTH_SHORT).show();
-                    }
-                });
+//                qty = Integer.parseInt(itemQty.getText().toString());
 
-                // Showing Alert Message
-                alertDialog.show();
+//                AlertDialog alertDialog = new AlertDialog.Builder(
+//                        NewRequisition.this).create();
+//
+//                alertDialog.setTitle("Alert Dialog");
+//                alertDialog.setMessage(itemID + "\n" + qty);
+//                alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        //Toast.makeText(getApplicationContext(), "You clicked on OK", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//                alertDialog.show();
 
             }
         });
@@ -98,7 +105,6 @@ public class NewRequisition extends ActionBarActivity {
         sendReqBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
 
             }
         });
@@ -112,7 +118,40 @@ public class NewRequisition extends ActionBarActivity {
         itemMap.put(5,"Clip Paper Medium");
         itemMap.put(6,"Clip Paper Small");
     }
-
-
-
 }
+
+class TempItem{
+    String iName;
+    int iQty;
+
+    public TempItem(String iName,int iQty){
+        this.iName = iName;
+        this.iQty = iQty;
+    }
+}
+
+class ItemsAdapter extends ArrayAdapter<TempItem> {
+    public ItemsAdapter(Context context, ArrayList<TempItem> items) {
+        super(context, 0, items);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        TempItem item = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_items, parent, false);
+        }
+
+        TextView tvItemName = (TextView) convertView.findViewById(R.id.itemName_txt);
+        TextView tvItemQty = (TextView) convertView.findViewById(R.id.itemQty_txt);
+
+        tvItemName.setText(item.iName);
+        tvItemQty.setText(Integer.toString(item.iQty));
+
+        return convertView;
+    }
+}
+
+
