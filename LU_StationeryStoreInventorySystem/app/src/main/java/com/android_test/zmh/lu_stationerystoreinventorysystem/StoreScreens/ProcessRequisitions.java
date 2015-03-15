8be61c60b70d8220b6c4bd0a95e1ef20b178bcd7
bluesneed <hd.kevinwong@gmail.com>
2015-03-15 15:24:19 +0800

    a little change

--- LU_StationeryStoreInventorySystem/app/src/main/java/com/android_test/zmh/lu_stationerystoreinventorysystem/StoreScreens/ProcessRequisitions.java
+++ LU_StationeryStoreInventorySystem/app/src/main/java/com/android_test/zmh/lu_stationerystoreinventorysystem/StoreScreens/ProcessRequisitions.java
@@ -1,9 +1,12 @@
 package com.android_test.zmh.lu_stationerystoreinventorysystem.StoreScreens;
 
+import android.app.DownloadManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.android.volley.Request;
@@ -11,6 +14,8 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.android_test.zmh.lu_stationerystoreinventorysystem.Adapter.ProcessRequisitionAdapter;
 import com.android_test.zmh.lu_stationerystoreinventorysystem.IPopulator.IItem;
@@ -18,10 +23,13 @@ import com.android_test.zmh.lu_stationerystoreinventorysystem.ModelPopulator.Ite
 import com.android_test.zmh.lu_stationerystoreinventorysystem.Models.Item;
 import com.android_test.zmh.lu_stationerystoreinventorysystem.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProcessRequisitions extends ActionBarActivity {
 
@@ -29,11 +37,17 @@ public class ProcessRequisitions extends ActionBarActivity {
     private RequestQueue mRequestQueue;
     private ArrayList<Item> retrivalItems;
     private String url = "https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=7c5c19eba3c21dc8bb16f00829b2be41&date=2015-02-09&format=json&nojsoncallback=1";
+    private String url2 = "http://10.10.1.202/LU_Store_MvcV1/api/purchase_orderApi/approve";
+    private Button button ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_process_requisitions);
+        button = (Button)findViewById(R.id.button);
+        setButtonOnclick();
+
+
         itemPopulator = new ItemPopulator();
         mRequestQueue = Volley.newRequestQueue(this);
         final ListView list = (ListView) findViewById(R.id.item_list);
@@ -88,5 +102,37 @@ public class ProcessRequisitions extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setButtonOnclick(){
+
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("orderID", "aa");
+        map.put("not the attribute", "aa");
+       JSONArray ja = new JSONArray();
+
+
+        final JSONObject jsonObject = new JSONObject(map);
+        button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               JsonRequest<JSONObject> jsonRequest = new JsonObjectRequest(Request.Method.POST,url2,jsonObject,
+                       new Response.Listener<JSONObject>() {
+                           @Override
+                           public void onResponse(JSONObject response) {
+                               System.out.println(response);
+                           }
+                       }, new Response.ErrorListener() {
+                   @Override
+                   public void onErrorResponse(VolleyError error) {
+                   }
+               })
+               {
+               };
+               mRequestQueue.add(jsonRequest);
+           }
+       });
+
     }
 }
